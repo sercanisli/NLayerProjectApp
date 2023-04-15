efa6961712040023f0f0c03f1384d6037710920e
Sercan ISLI <sercanislii@gmail.com>
2023-04-15 18:38:31 +0300

    Add project files.

---
+++ NLayerProject.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Core.UnitOfWorks
+{
+    public interface IUnitOfWork
+    {
+        Task ComitAsync();
+        void Comit();
+    }
+}
---
+++ NLayerProject.Repository/Context/AppDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayerProject.Core;
+using NLayerProject.Repository.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Repository.Context
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet <Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+
+
+        //To be accessed via product object
+        //public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //We accessed our configurations from here and run it.
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+
+            //modelBuilder.ApplyConfiguration(new ProductConfiguration());
+        }
+    }
+}
