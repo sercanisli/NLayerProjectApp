e5813f2aa2ab6eb4dfb001eafbb9bea4976f2a94
Sercan ISLI <sercanislii@gmail.com>
2023-04-18 17:38:53 +0300

    MVC Project is created. Product controller and save process are done. FluentValidation Applied. Update and Remove process are done. NotFoundFilter is done. Created Date and Update Date made dynamic.

--- NLayerProject.Service/Validations/FluentValidation/ProductDtoValidator.cs
+++ NLayerProject.Service/Validations/FluentValidation/ProductDtoValidator.cs
@@ -14,7 +14,6 @@ namespace NLayerProject.Service.Validations.FluentValidation
         {
             RuleFor(x => x.Name).NotNull().WithMessage("{PropertyName} is required").NotEmpty().WithMessage("{PropertyName} is required");
             RuleFor(x => x.Price).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater 0");
-            RuleFor(x => x.Stock).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater 0");
         }
     }
 }
